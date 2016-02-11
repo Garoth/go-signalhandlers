6b2e047c97895215c8cb8568a2e7ed2a91245e9d
Andrei Thorp <athorp@evernote.com>
2016-02-10 23:04:15 -0800

    Updates to comply to gofmt and commentary

--- signalhandlers.go
+++ signalhandlers.go
@@ -1,28 +1,29 @@
 package signalhandlers
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "os/signal"
-    "syscall"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+// Interrupt blocking listener that exits with success (0) when SIGINT is found.
 func Interrupt() {
-    ch := make(chan os.Signal)
-    signal.Notify(ch, syscall.SIGINT)
-    <-ch
-    fmt.Println("")
-    log.Println("CTRL-C (SIGINT); exiting")
-    os.Exit(0)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT)
+	<-ch
+	fmt.Println("")
+	log.Println("CTRL-C (SIGINT); exiting")
+	os.Exit(0)
 }
 
+// Quit blocking listener that exits with failure (1) when SIGQUIT is found.
 func Quit() {
-    ch := make(chan os.Signal)
-    signal.Notify(ch, syscall.SIGQUIT)
-    <-ch
-    fmt.Println("")
-    log.Println("CTRL-\\ (SIGQUIT); exiting")
-    os.Exit(1)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGQUIT)
+	<-ch
+	fmt.Println("")
+	log.Println("CTRL-\\ (SIGQUIT); exiting")
+	os.Exit(1)
 }
-
